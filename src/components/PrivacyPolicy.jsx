653d9bcb8a2355d50f23667a5dffdaf8b2ee0fa9
STharshan <sivatharshan81@gmail.com>
2025-09-12 17:11:38 +0530

    Add term

--- src/components/PrivacyPolicy.jsx
+++ src/components/PrivacyPolicy.jsx
@@ -111,7 +111,7 @@ const PrivacyPolicy = () => {
                     </p>
                     <div className="text-gray-700">
                         <p><strong>Empire Scaffolding</strong></p>
-                        <p>Email: <a href="mailto:info@empirescaffolding.co.uk" className="text-blue-600">info@empirescaffolding.co.uk</a></p>
+                        <p>Email: <a href="mailto:info@empirescaffolding.co.uk" className="text-blue-600 hover:underline">info@empirescaffolding.co.uk</a></p>
                         <p>Phone: 0115 964 1600</p>
                         <p>Address: Backerbrook Ind Est/Wigwam La, Nottingham NG15 7SZ</p>
                     </div>
